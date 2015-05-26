47bf440f96e8d1c64785c1381933a45c880e75a5
Olivier Dahindend <odahinden-radconsulting>
2015-05-26 21:20:09 +0200

    Added Maintenance Support

--- system/config/localconfig.php
+++ system/config/localconfig.php
@@ -24,4 +24,5 @@ $GLOBALS['TL_CONFIG']['imageWidth'] = 3000;
 $GLOBALS['TL_CONFIG']['imageHeight'] = 3000;
 $GLOBALS['TL_CONFIG']['disableCron'] = true;
 $GLOBALS['TL_CONFIG']['adminEmail'] = 'o.dahinden@rad-consulting.ch';
+$GLOBALS['TL_CONFIG']['allowedTags'] = '<a><abbr><acronym><address><area><article><aside><audio><b><bdi><bdo><big><blockquote><br><base><button><canvas><caption><cite><code><col><colgroup><data><datalist><dataset><dd><del><dfn><div><dl><dt><em><fieldset><figcaption><figure><footer><form><h1><h2><h3><h4><h5><h6><header><hgroup><hr><i><img><input><ins><kbd><keygen><label><legend><li><link><map><mark><menu><nav><object><ol><optgroup><option><output><p><param><pre><q><s><samp><section><select><small><source><span><strong><style><sub><sup><table><tbody><td><textarea><tfoot><th><thead><time><tr><tt><u><ul><var><video><wbr><script>';
 ### INSTALL SCRIPT STOP ###
